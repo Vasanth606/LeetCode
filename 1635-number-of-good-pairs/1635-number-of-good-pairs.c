87e8b6bd55f313d532ae6db418ce0807e8515b31
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2024-07-16 11:48:11 +0530

    Time: 4 ms (49.88%), Space: 7.9 MB (12.84%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.c
@@ -0,0 +1,8 @@
+int numIdenticalPairs(int* nums, int n) {
+    int count = 0;
+    for(int i = 0;i < n; i++)
+       for(int j = i + 1; j < n; j++)
+           if(nums[i] == nums[j]) count++;
+    
+    return count;
+}
