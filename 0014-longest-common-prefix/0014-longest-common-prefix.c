821a18b3a7dfefa0c3f1ff50b70a18058cce9d0c
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2024-07-16 10:27:42 +0530

    Time: 4 ms (35.33%), Space: 8 MB (5.59%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.c
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -4,9 +4,9 @@ char* longestCommonPrefix(char** strs, int strsSize) {
     if(strsSize < 2) return strs[0];
     for(int i = 0; i < strlen(strs[0]); i++){
         int flag=1;
-        for(int j = 1; j < strsSize; j++){
+        for(int j = 1; j < strsSize; j++)
             if(strs[0][i] != strs[j][i]) flag=0;
-        }
+        
         if(flag){
             res[i] = strs[0][i];
             res[i+1]='\0';
