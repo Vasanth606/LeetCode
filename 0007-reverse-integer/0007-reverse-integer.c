44d2ed3d81a8ed6b8447bc2bda56638cc504bbcd
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2024-07-16 10:21:12 +0530

    Time: 0 ms (100%), Space: 6.3 MB (5.83%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,15 @@
+int reverse(int x){
+
+
+
+    int sum=0;
+    while(x!=0){
+     if(sum>INT_MAX/10 || sum<INT_MIN/10) return 0;
+        sum = sum*10 + x%10;
+        x /= 10;
+    }
+
+    
+ return sum;  
+}
+
