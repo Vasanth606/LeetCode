58c40bdf136e40c4a7d883b4342264770cec9369
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2024-07-18 20:43:31 +0530

    Time: 5 ms (27.2%), Space: 8.1 MB (9.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.c
+++ 0242-valid-anagram/0242-valid-anagram.c
@@ -1,7 +1,10 @@
 bool isAnagram(char* s, char* t) {
     int seen[26] = {0};
-    for(int i = 0; i < strlen(s); i++) seen[s[i] - 97]++;
-    for(int i = 0; i < strlen(t); i++) seen[t[i] - 97]--;
+    if(strlen(s) != strlen(t)) return false;
+    for(int i = 0; i < strlen(s); i++){ 
+    seen[s[i] - 97]++;
+    seen[t[i] - 97]--;
+    }
     for(int i = 0; i < 26; i++)
         if(seen[i] != 0) return false;
     return true; 
