19ccf360e422f22472beb4ca61dbea1fe5e80563
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2024-07-22 09:20:22 +0530

    Time: 13 ms (10.99%), Space: 8.8 MB (6.59%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.c
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.c
@@ -1,12 +1,10 @@
 int mostWordsFound(char** sen, int n) {
     int count = 0, max = 0;
     for(int i = 0; i < n; i++){
-           count = 0;
+        count = 0;
         for(int j = 0; j < strlen(sen[i]); j++){
-           if(sen[i][j] != '\0') {
-                if(isspace(sen[i][j])) count++;
-           }
-           if(max < count) max = count;
+            if(sen[i][j] != '\0') if(isspace(sen[i][j])) count++;
+            if(max < count) max = count;
         }
     }
     return max+1;
