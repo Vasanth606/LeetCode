5966e98e4395043dd0d80d9a5854628fadbb2ee3
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2024-07-22 14:32:33 +0530

    Time: 4 ms (9.09%), Space: 7.8 MB (9.09%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.c
@@ -0,0 +1,31 @@
+int add(int n){
+   int sum = 0;
+    while(n){
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+int getLucky(char* s, int k) {
+    int sum = 0;
+    int seen[27] ={ 0 };
+    for(int i = 1;i < 27; i++)
+        seen[i] = i ;
+    for(int i = 0; s[i] != '\0'; i++){
+        if(seen[s[i] - 96] > 9) sum += add(seen[s[i] - 96]);
+        else sum += seen[s[i] - 96];
+    }
+    printf("%d",sum);
+    if( k == 1 ) return sum;
+    if(k == 2) {
+        sum = add(sum);
+        return sum;
+    }
+    else{
+        //  int temp = sum;
+        for(int i = k;i > 0; i--){
+            sum = add(sum);
+        }            
+    }
+    return sum;
+}
