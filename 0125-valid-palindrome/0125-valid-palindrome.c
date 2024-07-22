36ba056448cfd1ce0508fb73aa34811515e8c51b
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2024-07-22 16:25:42 +0530

    Time: 2 ms (62.37%), Space: 8.8 MB (5.13%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.c
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -1,27 +1,19 @@
-
 bool isPalindrome(char* s) {
     int len = strlen(s);
-    char str[len + 1]; // Include space for the null terminator
+    char str[len + 1]; 
     int idx = 0, c = 0;
-    
     for (int i = 0; i < len; i++) {
         if (isalnum(s[i])) {
-            str[idx++] = tolower(s[i]); // Store only alphanumeric characters in lowercase
+            str[idx++] = tolower(s[i]); 
         }
     }
-    
-    str[idx] = '\0'; // Null-terminate the string
-    
+    str[idx] = '\0'; 
     int start = 0;
     int end = idx - 1;
-    
     while (start < end) {
-        if (str[start] != str[end]) {
-            return false;
-        }
+        if (str[start] != str[end]) return false;
         start++;
         end--;
     }
-    
     return true;
 }
