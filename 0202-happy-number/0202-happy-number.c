f001cd6a08756a57633dc0071457fefdae53a79f
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2024-07-22 11:44:36 +0530

    Time: 3 ms (46.2%), Space: 7.6 MB (5.62%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.c
@@ -0,0 +1,15 @@
+bool isHappy(int n) {
+    int sum = 0, flag = 0;
+    if(n == 1) return true;
+    while( n > 4){
+     while(n != 0){
+       int r = n % 10;
+       n /= 10;
+       sum += r * r;
+    }
+    if(sum == 1) return true;
+    n = sum;
+    sum = 0;
+    }
+    return false;
+}
