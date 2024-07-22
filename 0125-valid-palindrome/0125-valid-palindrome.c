b84f13bda84674262d86bbe50ff0ba8a95ea7125
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2024-07-22 16:26:23 +0530

    Time: 6 ms (19.91%), Space: 8.8 MB (5.13%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.c
+++ 0125-valid-palindrome/0125-valid-palindrome.c
@@ -11,9 +11,12 @@ bool isPalindrome(char* s) {
     int start = 0;
     int end = idx - 1;
     while (start < end) {
-        if (str[start] != str[end]) return false;
+        if (str[start] != str[end]) {
+            return false;
+        }
         start++;
         end--;
     }
+
     return true;
 }
