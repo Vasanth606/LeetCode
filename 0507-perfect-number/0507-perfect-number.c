052a93e070339d015e76ff0c727c5fc75ff343bd
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2024-08-01 10:06:45 +0530

    Time: 3 ms (89.95%), Space: 7.8 MB (5.02%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.c
+++ 0507-perfect-number/0507-perfect-number.c
@@ -1,7 +1,9 @@
 bool checkPerfectNumber(int n) {
-    int sum = 0;
-    for(int i = 1; i <= n / 2; i++){
-        if(n % i == 0) sum += i;
-    }   
-    return sum == n;
+    // int sum = 0;
+    // for(int i = 1; i <= n / 2; i++){
+    //     if(n % i == 0) sum += i;
+    // }  
+    if(n == 6|| n == 28 || n == 496 || n == 8128 || n == 33550336) 
+    return 1;
+    return 0;
 }
