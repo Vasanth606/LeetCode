49950638a165b36c41466a4f1f43613591e9c1b1
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2024-10-22 15:49:55 +0530

    Time: 5 ms (94.22%), Space: 52.8 MB (98.13%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,13 +1,31 @@
 class Solution {
-    public List<Integer> findDuplicates(int[] nums) {
-        HashSet<Integer> num = new HashSet<>();
-        List<Integer> res = new ArrayList();
-        for(int i : nums){
-            if(num.contains(i)){
-                res.add(i);
+    public List<Integer> findDuplicates(int[] arr) {
+        // HashSet<Integer> num = new HashSet<>();
+        // List<Integer> res = new ArrayList();
+        // for(int i : nums){
+        //     if(num.contains(i)){
+        //         res.add(i);
+        //     }
+        //     num.add(i);
+        // } 
+        // return res;
+         int i = 0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                swap(arr,i,correct);
             }
-            num.add(i);
-        } 
-        return res;
+            else i++;
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int index = 0; index < arr.length; index++){
+            if(arr[index] != index + 1) ans.add(arr[index]);
+        }
+        return ans;
+    }
+    public static void swap(int[] arr, int f, int s){
+        int temp = arr[f];
+        arr[f] = arr[s];
+        arr[s] = temp;
     }
 }
