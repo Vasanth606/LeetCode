2044bb1796c12192321d617301bec71acc4050a1
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2025-01-21 10:18:49 +0530

    Time: 2 ms (97.61%), Space: 41.8 MB (64.9%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-        if(s.charAt(0) == ')' || s.charAt(0) == ']'||s.charAt(0) == '}')
-            return false;
+        
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
             if(c == '(' || c == '['|| c == '{'){
