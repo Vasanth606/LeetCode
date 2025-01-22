c2af1d59b6b644dcce3cac6d647c8a4da5965f4b
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2025-01-22 11:46:00 +0530

    Time: 4 ms (16.3%), Space: 56 MB (84.03%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,23 +1,17 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int totalGas = 0, totalCost = 0, currentGas = 0, startIndex = 0;
-
-        for (int i = 0; i < gas.length; i++) {
-            totalGas += gas[i];
-            totalCost += cost[i];
-            currentGas += gas[i] - cost[i];
-            // If currentGas is negative, reset the starting index and currentGas
-            if (currentGas < 0) {
-                startIndex = i + 1;
-                currentGas = 0;
+        int tG = 0, tC = 0, cG = 0, s = 0;
+        for(int i = 0; i < gas.length ; i++){
+            tG += gas[i];
+            tC += cost[i];
+            cG += gas[i] - cost[i];
+            if(cG < 0){
+                s = i + 1;
+                cG = 0;
             }
+            
         }
-
-        // If totalGas is less than totalCost, it's impossible to complete the circuit
-        if (totalGas < totalCost) {
-            return -1;
-        }
-
-        return startIndex;
+        if(tG < tC) return -1;
+        return s;
     }
 }
