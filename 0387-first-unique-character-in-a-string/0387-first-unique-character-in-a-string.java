1a29077d065fc721f815516d0deab9f6822ce881
Vasanth606 <143475761+Vasanth606@users.noreply.github.com>
2025-01-22 09:33:59 +0530

    Time: 34 ms (21.12%), Space: 45.5 MB (30.68%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -4,10 +4,10 @@ class Solution {
         for(char c : s.toCharArray()){
             m.put(c,m.getOrDefault(c,0) + 1);
         }
+        System.out.println(m);
         for(int i = 0; i < s.length(); i++){
             if(m.get(s.charAt(i)) == 1) return i;
         }
-        System.out.print(m);
         return -1;
     }
 }
